85258e4f7f82fb554b26ade536c2b6ab49fa1b20
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-02-23 11:40:23 -0600

    Actualizcación 11:40

--- SistemaOperativo.py
+++ SistemaOperativo.py
@@ -9,4 +9,15 @@ from random import uniform, Random
 #Cantidad del RAM del sistema Operativo
 cantidadRAM = 100  
 #Numero de proceso que se realizaran
-numProceso = 0
+capacidad_Proceso = 2
+#Memoria Requerida para la ejecucion:
+memRequerida = random.randint(1,10)
+#----Procesos READY----#
+cantidadInstrucciones = random.randint(1,10)
+
+
+class SistemOperativo(object):
+	def _init_(self,env):
+		#Creamos el espacio donde se guardara
+		self.RAM = simpy.Container(env, init=cantidadRAM, capacidad=capacidad_Proceso)
+		self.CPU = simpy.Resource(env, capacidad=capacidad_Proceso)
