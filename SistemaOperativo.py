dd3bbb1fa6a26e07e2cf105f442a55100d90f42d
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-02-23 11:03:27 -0600

    Actualizacion el 23 de febrero 2018
    Actualizacion

--- SistemaOperativo.py
+++ SistemaOperativo.py
@@ -2,3 +2,11 @@
 # Raul Monzon				17014
 # Descripcion: Simulacion de tiempo de corrida de un sistema operativo
 # Fecha: 23 de febrero 2018
+
+import simpy
+from random import uniform, Random 
+
+#Cantidad del RAM del sistema Operativo
+cantidadRAM = 100  
+#Numero de proceso que se realizaran
+numProceso = 200
